439871e23537bb1e0241b05eb598a3ec4da97c10
Marvin <sujithremi@gmail.com>
2025-08-05 20:22:42 +0530

    Added full backend functionality

--- run.py
+++
@@ -1,8 +0,0 @@
-# This file starts our Flask application server.
-from backend.app import create_app
-
-app = create_app()
-
-if __name__ == '__main__':
-    # We run in debug mode for development to get helpful error messages.
-    app.run(debug=True, port=5000)
