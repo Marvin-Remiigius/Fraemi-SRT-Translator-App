6505dc234d4e851ec72edc6a5c9d748eedd636a3
Joyshaju <joyshaju12345@gmail.com>
2025-08-05 15:50:04 +0530

    first

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,9 @@ import { useState } from 'react'
 import './style.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="bg-amber-100">Vite + React</h1>
     </>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './style.css'
 import App from './App.jsx'
+import Home from './home.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Home/>
     <App />
   </StrictMode>,
 )
